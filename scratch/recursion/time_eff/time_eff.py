feb5064059b3a03bd8b9756332bc3eb53232d58d
kelidas <kelidas@centrum.cz>
2013-05-22 10:38:49 +0200

    added new data and .npy->.npa renamed

--- scratch/recursion/time_eff/time_eff.py
+++ scratch/recursion/time_eff/time_eff.py
@@ -16,7 +16,8 @@ n_fil = 3.0
 lnx = -0.5
 x = .6  # np.exp(lnx)
 
-# data_1000 = np.load('tmp_1000.npy')
+
+# data_1000 = np.load('tmp_1000.npa')
 # data_1000_n = data_1000[:, 0]
 # data_1000_t = data_1000[:, 1]
 #
@@ -31,7 +32,7 @@ x = .6  # np.exp(lnx)
 #     data_1000_t = np.append(data_1000_t, np.array([t_mp]))
 #     data_1000_n = np.append(data_1000_n, np.array([n]))
 #     print n, g_mp
-#     np.save('tmp_1000.npy', np.vstack((data_1000_n, data_1000_t)).T)
+#     np.save('tmp_1000.npa', np.vstack((data_1000_n, data_1000_t)).T)
 #
 # plt.plot(data_1000_n, data_1000_t)
 # plt.show()
@@ -48,7 +49,7 @@ x = .6  # np.exp(lnx)
 #     print n, g_mp
 #
 # t_mp_lst = np.array(t_mp_lst)
-# np.save('tmp_10.npy', np.vstack((n_lst, t_mp_lst)).T)
+# np.save('tmp_10.npa', np.vstack((n_lst, t_mp_lst)).T)
 #
 # plt.plot(n_lst, t_mp_lst)
 
@@ -56,7 +57,7 @@ x = .6  # np.exp(lnx)
 
 # OLD version of recursion (naive)
 # t_lst = []
-# n_lst = np.arange(3., 19)
+# n_lst = np.arange(3., 18)
 # for n in n_lst:
 #     t = sysclock()
 #     g = Gn(x, scale, shape, n)
@@ -66,18 +67,20 @@ x = .6  # np.exp(lnx)
 #     print n, g
 #
 # t_lst = np.array(t_lst)
-# np.save('t_old.npy', np.vstack((n_lst, t_lst)).T)
+# np.save('t_old_mp.npa', np.vstack((n_lst, t_lst)).T)
 #
 # plt.plot(n_lst, t_lst)
 
 
 from scipy import stats
 
-data_old = np.load('t_old.npy')
-data_100 = np.load('tmp_100.npy')
-data_1000 = np.load('tmp_1000.npy')
-data_1000_gmpy = np.load('tmp_1000_gmpy.npy')
-data_10000 = np.load('tmp_10000.npy')
+data_old = np.load('t_old.npa')
+data_old_mp = np.load('t_old_mp.npa')
+data_old_c = np.load('t_old_c.npa')
+data_100 = np.load('tmp_100.npa')
+data_1000 = np.load('tmp_1000.npa')
+data_1000_gmpy = np.load('tmp_1000_gmpy.npa')
+data_10000 = np.load('tmp_10000.npa')
 
 print 'old'
 slope, intercept, r_value, p_value, std_err = stats.linregress(data_old[:, 0], np.log(data_old[:, 1]))
@@ -105,6 +108,8 @@ print 'slope =', slope, ', intercept =', intercept
 print 'coefficient of determination =', r_value ** 2
 
 plt.plot(data_old[:, 0], data_old[:, 1], label='old')
+plt.plot(data_old_mp[:, 0], data_old_mp[:, 1], label='old_mp')
+plt.plot(data_old_c[:, 0], data_old_c[:, 1], label='old_c')
 plt.plot(data_old[:, 0], data_old_fit, 'k--')
 
 plt.plot(data_100[:, 0], data_100[:, 1], label='100')
