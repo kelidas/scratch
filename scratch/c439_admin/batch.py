4a254a70d4f57802eb1eee617c089b9c9ef8aa7d
Václav Sadílek <kelidas@centrum.cz>
2014-04-25 09:44:12 +0200

    updated

--- scratch/c439_admin/batch.py
+++ scratch/c439_admin/batch.py
@@ -4,29 +4,46 @@ import sys
 import subprocess
 
 
-PC_LIST = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
+PC_LIST = [101, 102, 103, 104, 105, 106, 40, 108, 109, 110, 111, 112, 113, 114,
            115, 116, 117, 118, 119, 121, 122, 123, 124, 125, 126]
-
+# PC_LIST = [40]
 for i in PC_LIST:
     print '#' * 10, i, '#' * 30
     # IPCONFIG
     # os.system('ssh Teacher@147.229.29.%i ipconfig' % i)
 
-    # RESTART
-    # os.system('ssh Teacher@147.229.29.%i restart -r now' % i)
+    # LIST DIR
+    # os.system('ssh Teacher@147.229.29.%i dir /cygdrive/d/StudentsData/' % i)
+
+    # REBOOT
+    # os.system('ssh Teacher@147.229.29.%i reboot -r now' % i)
 
     # SHUTDOWN
-    # os.system('ssh Teacher@147.229.29.%i shutdown -s now' % i)
+    # subprocess.call(['ssh', 'Teacher@147.229.29.%i' % i, 'shutdown -s'])
 
     # REMOTE DESKTOP
+    # rdesktop -u Teacher -p 2bD318xM -g 95% -PKD 147.229.29.101 -r clipboard:PRIMARYCLIPBOARD
     # subprocess.call(['rdesktop', '-uTeacher', '-p2bD318xM', '-g90%', '-PKD', '-rclipboard:PRIMARYCLIPBOARD', '147.229.29.%i' % i])
 
     # UPLOAD DESKTOP WALLPAPER
     # os.system('scp gen_wallpaper/wallpaper.jpg Teacher@147.229.29.%i:/cygdrive/c/Windows/Web/Wallpaper/Windows/wallpaper.jpg' % i)
 
+    # UPLOAD iTalc public key
+#     if i != 101:
+#         os.system('scp -r "iTALC" Teacher@147.229.29.%i:/cygdrive/c/ProgramData/' % i)
+
     # CLEAN DISK D ON STUDENT MACHINES
 #     if i != 101:
 #         subprocess.call(['ssh', 'Teacher@147.229.29.%i' % i, 'rm -r /cygdrive/d/*'])
+
+    # Create Student directory on DISK D ON STUDENT MACHINES
+#     if i != 101:
+#         subprocess.call(['ssh', 'Teacher@147.229.29.%i' % i, 'mkdir /cygdrive/d/StudentsData'])
+
+    # fix desktop icons (logout all computers)
+    # subprocess.call(['ssh', 'Teacher@147.229.29.%i' % i, 'rm /cygdrive/c/Users/Student/AppData/Local/IconCache.db'])
+    # subprocess.call(['ssh', 'Teacher@147.229.29.%i' % i, 'rm /cygdrive/c/Users/Teacher/AppData/Local/IconCache.db'])
+    # subprocess.call(['ssh', 'Teacher@147.229.29.%i' % i, 'rm /cygdrive/c/Users/Remote/AppData/Local/IconCache.db'])
 
 
 
--- scratch/c439_admin/gen_wallpaper/c439_wallpaper.py
+++ scratch/c439_admin/gen_wallpaper/c439_wallpaper.py
@@ -1,76 +1,81 @@
+# coding: utf-8
 import Image, ImageDraw, ImageFont
 from os import chdir, path
 
-def txt2img(text,x=10,y=10,bg="#ffffff",fg="#000000",font="Verdana.ttf",FontSize=14):
-    font_dir = "/usr/share/fonts/truetype/msttcorefonts/"
-    img_name = "Win7 LtBlue 1920x1200_new.jpg"
-    font_size = FontSize
-    fnt = ImageFont.truetype(font_dir+font, font_size)
-    lineWidth = 20
-    img = Image.open("Win7 LtBlue 1920x1200.jpg")
-    #imgbg = Image.new('RGBA', img.size, "#000000") # make an entirely black image
-    #mask = Image.new('L',img.size,"#000000")       # make a mask that masks out all
-    draw = ImageDraw.Draw(img)                     # setup to draw on the main image
-    #drawmask = ImageDraw.Draw(mask)                # setup to draw on the mask
-    #drawmask.line((0, lineWidth/2, img.size[0],lineWidth/2),
-    #              fill="#999999", width=10)        # draw a line on the mask to allow some bg through
-    #img.paste(imgbg, mask=mask)                    # put the (somewhat) transparent bg on the main
-    draw.text((x,y), text, font=fnt, fill=bg)      # add some text to the main
-    del draw 
-    img.save(img_name,"JPEG",quality=100)  
 
-#txt2img('Ahoj')
+import textwrap
+
+def text_block(text, width, x, y, fill):
+    lines = textwrap.wrap(text, width=width)
+    print lines
+    y_text = y
+    for line in lines:
+        width, height = fnt.getsize(line)
+        draw.text(((x + width) / 2, y_text), line, font=fnt, fill=fill)
+        y_text += height
+
+
 
 
 
 font_dir = "/usr/share/fonts/truetype/msttcorefonts/"
 font_size = 30
-font = "Verdana.ttf"
-bg="#ffffff"
-fg="#000000"
-fnt = ImageFont.truetype(font_dir+font, font_size)
+font = "Arial.ttf"
+bg = "#ffffff"
+fg = "#000000"
+fnt = ImageFont.truetype(font_dir + font, font_size)
 lineWidth = 20
 
-for i in range(8):
-    img = Image.open("Win7 LtBlue 1920x1200.jpg")
-    #imgbg = Image.new('RGBA', img.size, "#000000") # make an entirely black image
-    #mask = Image.new('L',img.size,"#000000")       # make a mask that masks out all
-    draw = ImageDraw.Draw(img)                     # setup to draw on the main image
-    #drawmask = ImageDraw.Draw(mask)                # setup to draw on the mask
-    #drawmask.line((0, lineWidth/2, img.size[0],lineWidth/2),
-    #              fill="#999999", width=10)        # draw a line on the mask to allow some bg through
-    #img.paste(imgbg, mask=mask)                    # put the (somewhat) transparent bg on the main
 
-    x = 100
-    y = 100
-    dy = 15
-    draw.text((x,y), 'Save your data to disk D:', font=fnt, fill=bg)      # add some text to the main
-    y = y+dy+font_size
-    draw.text((x,y), 'All data on the Desktop will be deleted!', font=fnt, fill='#ff0000')
-    y += dy+font_size+60
-    draw.text((x,y), 'When leaving switch the computer', font=fnt, fill='#00ff00')
-    y += dy+font_size
-    draw.text((x,y), 'into Standby (Sleep) mode.', font=fnt, fill='#00ff00')
+img = Image.open('Win7 LtBlue 1600x1200.jpg')
+# imgbg = Image.new('RGBA', img.size, "#000000") # make an entirely black image
+# mask = Image.new('L',img.size,"#000000")       # make a mask that masks out all
+draw = ImageDraw.Draw(img)  # setup to draw on the main image
+# drawmask = ImageDraw.Draw(mask)                # setup to draw on the mask
+# drawmask.line((0, lineWidth/2, img.size[0],lineWidth/2),
+#              fill="#999999", width=10)        # draw a line on the mask to allow some bg through
+# img.paste(imgbg, mask=mask)                    # put the (somewhat) transparent bg on the main
 
-    x = 1000
-    y = 100
-    dy = 15
-    draw.text((x,y), 'Remote Desktop Acces:', font=fnt, fill=bg)
-    y += dy+font_size
-    draw.text((x,y), 'address: stm-%i.stm.fce.vutbr.cz'%(i+1), font=fnt, fill=bg)
-    y += (dy+font_size)
-    draw.text((x,y), 'login: student%i'%(i+1), font=fnt, fill=bg)
-    y += (dy+font_size)
-    draw.text((x,y), 'access from home: http://wifigw.cis.vutbr.cz#vpn', font=fnt, fill=bg)
-    y += (dy+font_size) + 45
-    draw.text((x,y), 'Wake on lan:', font=fnt, fill=bg)
-    y += (dy+font_size)
-    draw.text((x,y), 'http://ws_cheetah.stm.fce.vutbr.cz/wakeonlan/', font=fnt, fill=bg)
-    y += (dy+font_size) + 45
-    draw.text((x,y), u'Standby (Sleep) mode, reboot: Ctrl+Alt+End', font=fnt, fill=bg)
+x = 350
+y = 100
+dy = 15
+draw.text((x, y),
+          ur'- Data ukládejte do složky D:\StudentsData\<jméno v informačním systému FAST>',
+          font=fnt, fill=bg)  # add some text to the main
+y = y + dy + font_size
+draw.text((x, y),
+          ur'    - např. D:\StudentsData\novakj21\ ',
+          font=fnt, fill=bg)  # add some text to the main
+y = y + dy + font_size
+draw.text((x, y),
+          ur'    - data mimo tuto složku mohou být odstraněna',
+          font=fnt, fill=bg)  # add some text to the main
+y = y + dy + font_size + 60
 
-    del draw 
-    
-    img_name = "Win7 LtBlue 1920x1200_new_%i.jpg"%(i+1)
-    img.save(img_name,"JPEG",quality=100)  
-    del img
+draw.text((x, y),
+          ur'- Plocha se při odhlášení / vypnutí počítače automaticky maže!',
+          font=fnt, fill='#ff0000')
+draw.text((x + 1, y + 1),
+          ur'- Plocha se při odhlášení / vypnutí počítače automaticky maže!',
+          font=fnt, fill='#ff0000')
+y += dy + font_size
+draw.text((x, y),
+          ur'- Svá data si zálohujte, disk D může být kdykoliv smazán a data nejsou zálohována.',
+          font=fnt, fill='#00ff00')
+y += dy + font_size
+draw.text((x, y),
+          ur'    - (konec semestru, porucha počítače/systému)',
+          font=fnt, fill='#00ff00')
+
+y += dy + font_size + 60
+draw.text((x, y),
+          ur'- O případných problémech s počítačem informujte vyučujícího.',
+          font=fnt, fill=bg)
+
+
+
+del draw
+
+img_name = "wallpaper.jpg"
+img.save(img_name, "JPEG", quality=100)
+del img
