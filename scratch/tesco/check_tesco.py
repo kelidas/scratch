aed82b78d9aeae740b53654bf3b09b903842790d
Václav Sadílek <kelidas@centrum.cz>
2018-02-09 23:45:03 +0100

    added new items

--- scratch/tesco/check_tesco.py
+++ scratch/tesco/check_tesco.py
@@ -7,11 +7,16 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.dates import DayLocator, HourLocator, DateFormatter
+from collections import OrderedDict
 import datetime
 
 base_url = 'https://nakup.itesco.cz/groceries/cs-CZ/search?query='
-search = {'pampers3': 'pampers+midi+dry',
+search = {'pampers2': 'pampers+mini+dry',
+          'pampers2premium': 'pampers+mini+premium',
+          'pampers3': 'pampers+midi+dry',
+          'pampers3premium': 'pampers+midi+premium',
           'pampers4': 'pampers+maxi+dry',
+          'pampers4premium': 'pampers+maxi+premium',
           'hami_kase': 'hami+mlecna+kase'}
 directory = os.path.join(os.path.dirname(__file__), 'data')
 
@@ -54,7 +59,7 @@ for name, srch in search.items():
     time_new = time.tolist() + [datetime.datetime.now()]
     np.save(fname + '.npy', p_new)
     np.save(fname + '_time.npy', time_new)
-    print(name, p_last, p, p_last > p)
+    #print(name, p_last, p, p_last > p)
 
     fig, ax = plt.subplots(figsize=(6, 2), tight_layout=True)
     ax.plot(time_new, p_new)
@@ -64,7 +69,7 @@ for name, srch in search.items():
     fig.autofmt_xdate()
     fig.savefig(fname + '.png')
 
-    body += '<h3>old={}, new={}, {}</h3>'.format(p_last, p, p_last > p)
+    body += '<h3>min={}, new={}, {}</h3>'.format(min(p_new), p)
     body += '<img src={}.png><br>'.format(name)
 
 with open(os.path.join(directory, 'prices.html'), 'w') as f:
